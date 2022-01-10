c3fc0f0df7f817add9eed2c0ef57828c5d55e67d
Sithanga Uwage <suwage@geometricresults.co.uk>
2022-01-10 16:56:44 +0000

    Section 3 and go by example

---
+++ goByExample/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is a " + "string")
+	fmt.Println("2 + 2 = ", 2+2)
+	fmt.Println("4.0 / 5.0 = ", 4.0/5.0)
+	fmt.Println(true)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
